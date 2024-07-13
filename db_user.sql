7d685828b2343bce525bd757e9f21ea8e069960f
Andre <andrecustodioes@gmail.com>
2024-07-13 23:31:46 +0000

    a

--- db_user.sql
+++ db_user.sql
@@ -2,3 +2,8 @@ CREATE USER 'shoppy'@'localhost' IDENTIFIED BY 'System32';
 
 GRANT INSERT, UPDATE, DELETE, SELECT, REFERENCES 
 on wine_shop.* TO 'shoppy'@'localhost' WITH GRANT OPTION;
+
+CREATE USER 'shoppyadmin'@'localhost' IDENTIFIED BY 'System32';
+
+GRANT CREATE, ALTER, DROP, INSERT, UPDATE, DELETE, SELECT, REFERENCES, 
+RELOAD on wine_shop.* TO 'shoppyadmin'@'localhost' WITH GRANT OPTION;
--- phpmyadmin.bash
+++ phpmyadmin.bash
@@ -2,6 +2,8 @@
 
 apt update
 
+apt install phpmyadmin -y
+
 mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED VIA mysql_native_password"
 
 mariadb -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('System32')"
@@ -13,12 +15,6 @@ mariadb -u root -pSystem32 -e "DROP USER ''@'$(hostname)'"
 mariadb -u root -pSystem32 -e "DROP DATABASE test"
 
 mariadb -u root -pSystem32 -e "FLUSH PRIVILEGES"
-
-mariadb -u root -pSystem32 -e "UNINSTALL COMPONENT 'file://component_validate_password'"
-
-apt install phpmyadmin -y
-
-mariadb -u root -pSystem32 -e "INSTALL COMPONENT 'file://component_validate_password'"
 
 ln -s /usr/share/phpmyadmin /var/www/phpmyadmin
 
