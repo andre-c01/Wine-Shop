282b75f749bb934ad3c2bf37ce7076fcc903ede9
Andre <andrecustodioes@gmail.com>
2024-07-13 23:39:59 +0000

    d

--- db_user.sql
+++ db_user.sql
@@ -1,3 +1,6 @@
+DROP USER IF EXISTS shoppy@'localhost';
+DROP USER IF EXISTS shoppyadmin@'localhost';
+
 CREATE USER 'shoppy'@'localhost' IDENTIFIED BY 'System32';
 
 GRANT INSERT, UPDATE, DELETE, SELECT, REFERENCES 
@@ -5,5 +8,5 @@ on wine_shop.* TO 'shoppy'@'localhost' WITH GRANT OPTION;
 
 CREATE USER 'shoppyadmin'@'localhost' IDENTIFIED BY 'System32';
 
-GRANT CREATE, ALTER, DROP, INSERT, UPDATE, DELETE, SELECT, REFERENCES, 
-RELOAD on wine_shop.* TO 'shoppyadmin'@'localhost' WITH GRANT OPTION;
+GRANT CREATE, ALTER, DROP, INSERT, UPDATE, DELETE, SELECT, REFERENCES
+ on wine_shop.* TO 'shoppyadmin'@'localhost' WITH GRANT OPTION;
